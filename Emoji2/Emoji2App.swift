d6e621312b0ceba140db0a4d857922e2d8232dc8
trevanian66 <kwablah@hotmail.com>
2024-04-07 16:31:06 +0100

    Initial Commit

---
+++ Emoji2/Emoji2App.swift
@@ -0,0 +1,17 @@
+//
+//  Emoji2App.swift
+//  Emoji2
+//
+//  Created by Kobby Awadzi on 07/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct Emoji2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
